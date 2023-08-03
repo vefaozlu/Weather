16c98595b07e5d8e5c00aefc6bcbbc46d32270a4
Vefa Ozlu <vefaozlu01@gmail.com>
2023-08-03 22:20:01 +0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Vefa Ozlu on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
